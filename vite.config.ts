56f8efd0e23eb10e0f0c23a17761394a6bb0c89c
Timur <timengalytchev@mail.ru>
2024-06-09 12:27:52 +0300

    delete webapp

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+    }
+  },
 
   define: {
     __IS_DEV__: JSON.stringify(true),
